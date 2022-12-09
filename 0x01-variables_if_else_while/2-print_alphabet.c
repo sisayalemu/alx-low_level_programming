f6824e24ed71cf8a9b3e54ed8eb97be3c60923ed
kiya-koo <kiyakoo44@gmail.com>
2022-12-09 00:41:03 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+
+int main(void)
+{
+	char a_z = 'a';
+
+	while (a_z <= 'z')
+	{
+		putchar(a_z);
+		a_z++;
+
+	}
+
+	putchar('\n');
+	return (0);
+
+}
