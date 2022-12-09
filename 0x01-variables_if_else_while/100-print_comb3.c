e4dccf25d522742816981934b0f3a9e55085b7df
kiya-koo <kiyakoo44@gmail.com>
2022-12-09 09:41:25 -0800

    task 10 new update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-int nl;
-int nr;
 
-for (nl = 48; nl <= 56; nl++)
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (success)
+ *     */
+int main(void) 
 {
-for (nr = nl + 1; nr <= 57; nr++)
-{
-	putchar(nl);
-	putchar(nr);
-	if (nl != 56 || nr != 57)
-	{
-		putchar(',');
-		putchar(32);
-	}	
-}
-}
-putchar('\n');
-return (0);
-}
+  int nl;
+  int nr;
+
+ for (nl = 48; nl <= 56; nl++) 
+ {
+	for (nr = nl + 1; nr <= 57; nr++)
+        {
+	       	putchar(nl);
+	        putchar(nr);
+	       	if (nl != 56 || nr != 57)
+	       	{
+			putchar(',');
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+ }
