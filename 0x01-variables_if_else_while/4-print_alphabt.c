9fbea411885443e1aa78d911dc4ebd155e707ea6
kiya-koo <kiyakoo44@gmail.com>
2022-12-09 02:46:56 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a == 'e' && a == 'q')
+			continue;
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
