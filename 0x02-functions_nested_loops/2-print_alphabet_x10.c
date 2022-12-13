91ab7bd965cfc4279826825c7044d82263053791
kiya-koo <kiyakoo44@gmail.com>
2022-12-13 12:06:50 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - this is a function
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int x = 0;
+
+	while (b < 10)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
