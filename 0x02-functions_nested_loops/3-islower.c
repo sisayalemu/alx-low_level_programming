3adf10a33148a35483cb11898ffddc6a89447ed0
kiya-koo <kiyakoo44@gmail.com>
2022-12-13 12:18:52 -0800

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
  *_islower - lowercase function checker
+ *@c: letter tested
  * 
  * Return: Always 0 (success)
  */
+
 int _islower(int c)
 {
+
 	if (c >= 97 && c <= 122)
 	
 		return (1);
