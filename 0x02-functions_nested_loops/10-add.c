8456f48de0dba93a9b1325ffbc1b6e86fca1c9df
kiya-koo <kiyakoo44@gmail.com>
2022-12-14 09:38:15 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add - this is a function that add 2 number
+ *@x: the first variable being added
+ *@y: the second variable being added
+ *Return: Always 0 (success0
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+
+	return (sum);
+}
