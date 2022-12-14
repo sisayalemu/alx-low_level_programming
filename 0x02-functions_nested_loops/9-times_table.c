be3985f4430559ccf18829ee1a888d67dffae20f
kiya-koo <kiyakoo44@gmail.com>
2022-12-14 09:30:20 -0800

    task 9 update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,11 +8,11 @@ void times_table(void)
 	int x;
 	int y;
 
-	for (x = 0;x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 	{
 		_putchar('0');
 
-		for (y = 1;y <= 9; y++)
+		for (y = 1; y <= 9; y++)
 		{
 			int output = x * y;
 
