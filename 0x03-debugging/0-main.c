48db80f53c64b6a8d61e12a01700478355d38109
kiya-koo <kiyakoo44@gmail.com>
2022-12-15 10:52:42 -0800

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *main - test func for pstive or negative
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postive_or_negative(i);
+
+	return (0);
+}
