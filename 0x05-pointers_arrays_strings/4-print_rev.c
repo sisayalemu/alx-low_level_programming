ab91ded24ac3f00f2c7a7470d8751c3b5cce82ef
kiya-koo <kiyakoo44@gmail.com>
2022-12-20 05:27:49 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - a function that reverse a string
+ * @s: variable being store a string
+ */
+void print_env(char *s)
+{
+	int i = 0;
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
