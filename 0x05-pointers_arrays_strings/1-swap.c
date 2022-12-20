a88cf743849f606e6a10a4899a06addd5fb74850
kiya-koo <kiyakoo44@gmail.com>
2022-12-20 05:02:27 -0800

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- *swap_int - swap two integers 
- *@a: variable a to be tested
- *@b: variable b to be tested
+ * swao_int - function that swap two number
+ * @a: var a to be inputed
+ * @b: var b to be inputed
  */
 void swap_int(int *a, int *b)
 {
 	int swap;
 
 	swap = *a;
-	a* = *b;
+	*a = *b;
 	*b = swap;
 }
