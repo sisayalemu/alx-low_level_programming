3671ef684733fccb5583346d93d0c01543ef8272
kiya-koo <kiyakoo44@gmail.com>
2022-12-20 03:56:57 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - reset the value of n
+ * @n: the number to modify
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is c pointer and string project
