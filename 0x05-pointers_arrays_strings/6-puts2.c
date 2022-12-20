16fa02864020c5fdbeffadf2f7770f74d0aa5ac9
kiya-koo <kiyakoo44@gmail.com>
2022-12-20 06:24:12 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - print every charcter of a string
+ * @str: variable being used
+ */
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(str[a]);
+		}
+		a++;
+	}
+	_putchar('\n');
+}
