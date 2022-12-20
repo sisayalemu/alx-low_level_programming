3d98384a1f9fc65dd5d2d93039e3487ea199e9bc
kiya-koo <kiyakoo44@gmail.com>
2022-12-20 08:28:41 -0800

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - results in second half of the string
+ * @str: variable to be used
+ */
+void puts_half(char *str)
+{
+	int len, x, a;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+
+	x = (len - 1) / 2;
+
+	for (a = n + 1; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+
+	_putchar('\n');
+}
