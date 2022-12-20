3a5bb9f38118f24ed51e19b6e4090b2cec5f288e
kiya-koo <kiyakoo44@gmail.com>
2022-12-20 09:53:46 -0800

    task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+/**
+ * main - a function printing a password
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int asc;
+	int a;
+	int b;
+	int randm;
+	char pass[100];
+	time_t t;
+
+	a = 0;
+
+	srand((int) time(&t));
+
+	while (asc > 126)
+	{
+		randm = rand() % 126;
+		pass[a] = randm;
+		asc -= randm;
+		a++;
+	}
+	if (asc > 0)
+	{
+		pass[a] = asc;
+	}
+	else
+	{
+		a--;
+	}
+
+	for (b = 0; b <= a; b++)
+	{
+		printf("%c", pass[b]);
+	}
+
+	return (0);
+}
