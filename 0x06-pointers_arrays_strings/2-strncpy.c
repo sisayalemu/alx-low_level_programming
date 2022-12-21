e220b8ae355d35bf618fb32634a537c185955b39
kiya-koo <kiyakoo44@gmail.com>
2022-12-21 09:34:42 -0800

    yask 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncpy - this is a function that copies a string
+ * @src: variable to be used as a source
+ * @dest: variable used as a destination
+ * @n: used as a num variable
+ * Return: nothing to return
+ */
+char *_strcpy(char *dest, char *src, int n)
+{
+	int a;
+
+	a = 0;
+
+	while (src[a] != '\0' && a < n)
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
+	return (dest);
+}
