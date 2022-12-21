18972235365037f4762b7a7091c0384a9a470d1f
kiya-koo <kiyakoo44@gmail.com>
2022-12-21 09:17:09 -0800

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - a function for a purpose of two strings
+ * @str: the source var for the func
+ * @dest: the destination var for the func
+ * @n: var to be used for the number var
+ * Return: it will return the concated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = strlen(dest);
+
+	int x = 0;
+
+	while (x < n && *src)
+	{
+		dest[i + x] = *src;
+		src++;
+		x++;
+	}
+
+	dest[i + x] = '\0';
+
+	return (dest);
+}
