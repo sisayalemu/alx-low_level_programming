2973c72990a2413e512a77bb8aff26fd9f43ba48
kiya-koo <kiyakoo44@gmail.com>
2023-01-03 07:14:41 -0800

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#iclude "main.h"
+
+/**
+ * print_chessboard - function that prints the chessboard
+ * @a: variable for the matrix
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int b, c;
+
+	for (b = 0; b < 8; b++)
+	{
+		for (c = 0;  c < 8; c++)
+		{
+			_putchar(*(*(b + a) + c));
+		}
+	_putchar('\n');
+	}
+}
