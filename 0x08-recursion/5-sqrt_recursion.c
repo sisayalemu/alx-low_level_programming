454700b6a7de834e41940ebf03ea328a8b21c28c
kiya-koo <kiyakoo44@gmail.com>
2023-01-04 08:55:52 -0800

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - function that returns the sqrt of num
+ * @n: variable for integer
+ * Return: result for sqrt integer
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - recursion func for _sqrt_recursion
+ * @n: variable for integer
+ * @a: variable for integer
+ * Return: sqrt of the number
+ */
+
+int _sqrt(int n, int a)
+{
+	if (n < 0)
+		return (-1);
+	else if ((a * a) > n)
+		return (-1);
+	else if (a * a == n)
+		return (a);
+
+	return (_sqrt(n, a + 1));
+}
