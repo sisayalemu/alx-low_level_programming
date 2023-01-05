f5a5d134d20a813e4ad01a368cbb10c32468b8b8
kiya-koo <kiyakoo44@gmail.com>
2023-01-05 06:52:56 -0800

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,19 +1,33 @@
 #include "main.h"
-int is_divisible(int number, int div);
-int is_prime_number(int n);
 /**
- * is_divisible - func for checking divisiblity
- * @number: variable for integer
- * @div: variable for integer
- * Return: output 0 or 1
+ * find_prime - a function for finding for prime
+ * @x: variable foe integer input
+ * @y: variable for integer input
+ * Return: it outputs a boolean value
  */
-
-int is_divisible(int number, int div)
+int find_prime(int x, int y)
 {
-	if (number % 2 == 0)
+	if (x % y == 0)
+	{
 		return (0);
-	if (div == number / 2)
-		return (1);
-
-	return (is_divisible(div + 1));
+	}
+	if (y == 1)
+	{
+		return (0);
+	}
+	
+	return (find_prime(x, y - 1));
+}
+/**
+ * is_prime_number - function for printing prime numbers
+ * @n: variable for  integer
+ * Return: prime num after recursion
+ */
+int is_prime_number(int n)
+{
+	if (n < 3)
+	{
+		return (0);
+	}
+	return (find_prime(n, n - 1));
 }
