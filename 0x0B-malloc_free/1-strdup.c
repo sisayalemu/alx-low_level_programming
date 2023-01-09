75a9cf6aba1557e251fe51d53d911cb405fa0a98
kiya-koo <kiyakoo44@gmail.com>
2023-01-09 08:32:29 -0800

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - function that returns a pointer
+ * @str: variable for char
+ *
+ * Return: pointer of an array
+ */
+
+char *_strdup(char *str)
+{
+	char *text;
+	int a, length = 0;
+
+	text = malloc(sizeof(char) * (length + 1));
+
+	if (str == NULL || text == NULL)
+		return (NULL);
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		text[a] = str[a];
+		length++;
+	}
+
+	text[length] = '\0';
+
+	return (text);
+}
