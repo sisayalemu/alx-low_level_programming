4911f464541c518a3fb69da95a774964bed1850c
kiya-koo <kiyakoo44@gmail.com>
2023-01-13 11:30:24 -0800

    task 6

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,22 +9,23 @@
  */
 int main(int argc, char *argv[])
 {
-unsigned long multi;
+unsigned long mul;
 int a, b;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (a = 1; a < argc; a++)
-	{
-		for (b = 0; argv[a][b] != '\0'; b++)
-		{
-			if (argv[a][b] > 57 || argv[a][b] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
+        if (argc != 3)
+        { printf("Error\n");
+        exit(98); }
+        for (a = 1; a < argc; a++)
+        {
+                for (b = 0; argv[a][b] != '\0'; b++)
+                {
+                        if (argv[a][b] > 57 || argv[a][b] < 48)
+                        {  printf("Error\n");
+                        exit(98); }
+                }
 
-	}
-	multi = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", multi);
+        }
+        mul = atol(argv[1]) *  atol(argv[2]);
+        printf("%lu\n", mul);
 return (0);
 }
+
