9b7b69af86b86fdaf1100fb0c0a2f9b92cf4c700
kiya-koo <kiyakoo44@gmail.com>
2023-01-13 10:34:34 -0800

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,41 +1,52 @@
-#include "main.h"
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * string_np - function that penates two strings
- * @s1: variable for the first string
- * @s2: variable for the second string
- * @n: variable for integer
- * Return: the concacnated string
+ * _strlen - function that calculate string lengthgth
+ * @string: variable for string
+ * Return: string lengthgth
  */
-char *string_np(char *s1, char *s2, unsigned int n)
+int _strlen(char *string)
+{
+	int a;
+
+	for (a = 0; string[a] != '\0'; a++)
+		;
+	return (a);
+}
+/**
+ * string_nconcat - concatenate s1 and n bytes of s2; return p to string
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n bytes to concat from string 2
+ * Return: pointer to concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int length = n, a;
+	int value, length, b, c;
+
+	value = n;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
+	if (value < 0)
+		return (NULL);
+	if (value >= _strlen(s2))
+		value = _strlen(s2);
 
-	for (a = 0; s1[a]; a++)
-		length++;
+	length = _strlen(s1) + value + 1;
 
-	p = malloc(sizeof(char) * (length + 1));
-
+	p = malloc(sizeof(*p) * length);
 	if (p == NULL)
 		return (NULL);
 
-	length = 0;
-
-	for (a = 0; s1[a]; a++)
-		p[length++] = s1[a];
-
-	for (a = 0; s2[a] && a < n; a++)
-		p[length++] = s2[a];
-
-	p[length] = '\0';
+	for (b = 0; s1[b] != '\0'; b++)
+		p[b] = s1[b];
+	for (c = 0; c < value; c++)
+		p[b + c] = s2[c];
+	p[b + c] = '\0';
 
 	return (p);
 }
