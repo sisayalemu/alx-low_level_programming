33f18fe76d826f66b118eb21fe37c3e5aaee9eef
kiya-koo <kiyakoo44@gmail.com>
2023-01-13 10:39:49 -0800

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+  * _calloc - function that allocates memory
+  * @nmemb: variable for integer
+  * @size: variable for integer
+  *
+  * Return: allocated memory
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int a;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (a = 0; a < (nmemb * size); a++)
+	{
+		*((char *)(ptr) + i) = 0;
+	}
+
+	return (ptr);
+}
