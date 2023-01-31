0a8979f68fbd02296a3597892e8c200c07d6b27d
kiya-koo <kiyakoo44@gmail.com>
2023-01-31 03:06:44 -0800

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/*
+ * free_listint2 - finction that free a list
+ * @head: variable for pointing for first node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		node = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+}
