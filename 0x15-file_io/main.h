b3ea6024a7e1caed9d734a39a0b7b86778575d7a
kiya-koo <kiyakoo44@gmail.com>
2023-02-06 09:36:46 -0800

    task 0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_N
-
+#define MAIN_H
 #include <elf.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,14 +7,9 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-/**
- * ELF header functions
-*/
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
